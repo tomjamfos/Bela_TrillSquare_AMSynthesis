0c782c83ceab972b74f3a2ed3d1da465d9c6b306
tomjamfos <152189579+tomjamfos@users.noreply.github.com>
2024-07-20 14:44:18 +0100

    Add files via upload

---
+++ build/Wavetable.d
@@ -0,0 +1,5 @@
+/root/Bela/projects/my_trillSq_01/build/Wavetable.o: \
+  /root/Bela/projects/my_trillSq_01/Wavetable.cpp \
+  /root/Bela/projects/my_trillSq_01/Wavetable.h
+
+/root/Bela/projects/my_trillSq_01/Wavetable.h:
---
+++ build/render.d
@@ -0,0 +1,50 @@
+/root/Bela/projects/my_trillSq_01/build/render.o: \
+  /root/Bela/projects/my_trillSq_01/render.cpp include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  /usr/xenomai/include/cobalt/time.h include/Utilities.h \
+  libraries/Trill/Trill.h include/I2c.h \
+  /usr/xenomai/include/cobalt/fcntl.h \
+  /usr/xenomai/include/cobalt/sys/ioctl.h libraries/OnePole/OnePole.h \
+  libraries/Gui/Gui.h include/JSON.h include/JSONValue.h \
+  include/DataBuffer.h libraries/GuiController/GuiController.h \
+  libraries/GuiController/GuiSlider.h libraries/Scope/Scope.h \
+  /root/Bela/projects/my_trillSq_01/Wavetable.h
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+/usr/xenomai/include/cobalt/time.h:
+
+include/Utilities.h:
+
+libraries/Trill/Trill.h:
+
+include/I2c.h:
+
+/usr/xenomai/include/cobalt/fcntl.h:
+
+/usr/xenomai/include/cobalt/sys/ioctl.h:
+
+libraries/OnePole/OnePole.h:
+
+libraries/Gui/Gui.h:
+
+include/JSON.h:
+
+include/JSONValue.h:
+
+include/DataBuffer.h:
+
+libraries/GuiController/GuiController.h:
+
+libraries/GuiController/GuiSlider.h:
+
+libraries/Scope/Scope.h:
+
+/root/Bela/projects/my_trillSq_01/Wavetable.h:
